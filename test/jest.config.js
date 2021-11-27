e1ae08ab66c9124a7f6ecfd7a653e34a877f3b59
Alpha Olomi <alphaolomi@gmail.com>
2021-11-27 07:28:02 +0300

    fix jest config

--- test/jest.config.js
+++ test/jest.config.js
@@ -3,8 +3,18 @@ const { compilerOptions } = require("../tsconfig.json");
 
 const paths = compilerOptions.paths ? compilerOptions.paths : {};
 
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    rootDir: "./",
+    rootDir: "../",
+    testEnvironment: "jsdom",
+    moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
+    transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
+    // transform: {
+    //     "^.+\\.(ts|tsx)$": "ts-jest",
+    // },
+    
+    preset: 'ts-jest',
+    // testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
     setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
     testPathIgnorePatterns: [
         "<rootDir>/.next/",
@@ -17,3 +27,22 @@ module.exports = {
         "\\.(scss|sass|css)$": "identity-obj-proxy",
     },
 };
+
+// module.exports = {
+//     roots: ['<rootDir>'],
+//     testEnvironment: 'jsdom',
+//     moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+//     testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+//     transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+//     transform: {
+//       '^.+\\.(ts|tsx)$': 'babel-jest',
+//     },
+//     watchPlugins: [
+//       'jest-watch-typeahead/filename',
+//       'jest-watch-typeahead/testname',
+//     ],
+//     moduleNameMapper: {
+//       '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+//       '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+//     },
+//   }
